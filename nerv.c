54bdb1ca7aae43a8a216ba5824b996b0b442e716
Determinant <ted.sybil@gmail.com>
2015-05-22 14:02:08 +0800

    remove unnecessary pushvalue

--- nerv.c
+++ nerv.c
@@ -7,7 +7,6 @@ extern void nerv_matrix_init(lua_State *L);
 
 int luaopen_libnerv(lua_State *L) {
     lua_newtable(L);
-    lua_pushvalue(L, -1);
     lua_setfield(L, LUA_GLOBALSINDEX, "nerv");
     nerv_point_init(L);
     nerv_matrix_init(L);
