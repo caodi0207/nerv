c2abcd032e9f64bdbf015567d76edfada55c8866
Determinant <ted.sybil@gmail.com>
2015-05-27 12:05:00 +0800

    ...

--- layer/sigmoid.lua
+++ layer/sigmoid.lua
@@ -5,10 +5,14 @@ function SigmoidLayer:__init(id, global_conf)
     self.gconf = global_conf
 end
 
+function SigmoidLayer:update(bp_err, input, output)
+    -- no params, therefore do nothing
+end
+
 function SigmoidLayer:propagate(input, output)
     output:sigmoid(input)
 end
 
 function SigmoidLayer:back_propagate(next_bp_err, bp_err, input, output)
-    next_bp_err:sigmoid(bp_err, output)
+    next_bp_err:sigmoid_grad(bp_err, output)
 end
